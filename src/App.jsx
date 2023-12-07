1366295561cec7ebf15dadd5844d0cd1a5cc06f5
Roberto Toribio <roberttoribio04@gmail.com>
2023-12-06 21:18:27 -0800

    Finished checkpoint 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import viteLogo from '/vite.svg'
 import './App.css'
-const API_KEY = import.meta.env.VITE_APP_API_KEY;
+const API_KEY = "40828f6623a246233b579aa60148e9dac95d282f6b6052b65646b322c9a81549"
 
 function App() {
+  const [list,setList] = useState(null);
+  
+  useEffect ( () =>{
+    const fetchAllCoinData = async () =>{
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" 
+        + API_KEY);
+      const json = await response.json();
+      console.log('before printin json')
+      console.log(json)
+      console.log('after printin json')
+      setList(json);
 
-
-  return (
-    
-   <>
-   </>
+     
+      
+    };
+    fetchAllCoinData().catch(console.error);
+  },[]);
+  console.log("API-KEY is", API_KEY)
+  if (list && list.Data){
+    console.log('printing inside if statment',list.Data)
+  }
+  //console.log('printing contents of list',list.Data)
+  return(
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+        <ul>
+          {list && Object.entries(list.Data).map(([coin]) =>
+          list.Data[coin].PlatformType === "blockchain" ? (
+          <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+           ) : null
+          )}
+        </ul>
+    </div>
   )
 }
 
